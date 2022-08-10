e3a399ce97de63e61b75cc18d2e0dc41baeba412
Angel Cortes <angelcortes@tutanota.com>
2022-08-10 14:32:03 -0700

    created projectController

--- server/controllers/projectControllers.js
+++ server/controllers/projectControllers.js
@@ -14,19 +14,47 @@ const createErr = (errInfo) => {
 const projectController = {};
 
 projectController.getProjects = (req, res, next) => {
- 
+  //request is a array of users project ids
+  const { userProjectIds } = req.body
+  //find all matching ids and return them
+  Project.find( { '_id': { $in: usersProjectIds}}, (err,projectList) => {
+    if(err) return next(createErr(err));
+    res.locals.projectData = projectList;
+    return next();
+  })
 }
 
 projectController.createProject = (req, res, next) => {
-  const { } = req.body;
-}
+  const { project_name, project_created_by, project_members, project_description, project_start_date, project_end_date } = req.body;
+  
+  Project.create({project_name,project_created_by, project_members, 
+    project_description, project_start_date, project_end_date})
+  .then(data => {
+    res.locals.newProject = data._id;
+    next();
+  })
+  .catch(err => {
+    next(createErr(err));
+  })
+};
 
 projectController.updateProject = (req, res, next) => {
-
+  const { _id, changes } = req.body;
+  const filterRequest = { _id };
+  Project.findOneAndUpdate(filterRequest, { changes })
 }
 
 projectController.deleteProject = (req, res, next) => {
-
+  const { _id } = req.body;
+  const deleteId = { _id };
+  Project.deleteOne(deleteId)
+  .then(deletedDocument => {
+    res.locals.projectData = deletedDocument;
+    next();
+  })
+  .catch( err => {
+    next(createErr(err));
+  })
 }
 
 
